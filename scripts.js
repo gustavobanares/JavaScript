f3ae2a1d59fc38614ba63d5704545c36e02c043e
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-20 23:57:42 -0300

    Type of datas

--- scripts.js
+++ scripts.js
@@ -4,3 +4,44 @@ console.log('Hello World JavaScript')
 console.error('Este é um erro!')
 console.warn('Este é um aviso!')
 console.log('Esta é uma mensagem!')
+
+// variáveis
+var x = 10
+
+// maneiras mais modernas de declarar variáveis
+let y = 20 
+const z = 7
+
+if (1){
+    let y = 55
+    console.log(y)
+}
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+// tipos de dados 
+const name = 'Gustavo'
+console.log(name)
+console.log(typeof name)
+
+const shirtsqtd = 7
+console.log(shirtsqtd)
+console.log(typeof shirtsqtd)
+
+const decimal = 10.8
+console.log(decimal)
+console.log(typeof decimal)
+
+const isapproved = false // true or false 
+console.log(isapproved)
+console.log(typeof isapproved)
+
+let surname = null
+console.log(surname)
+console.log(typeof surname)
+
+surname = 'Bañares'
+console.log(surname)
+console.log(typeof surname)
