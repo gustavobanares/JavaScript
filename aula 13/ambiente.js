d93416b93999b7c76d99cca795eee0d58e76ea23
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-08-12 21:55:41 -0300

    Learning how  use while, do and for in js

---
+++ aula 13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 7)
+
+ar
+
+/*
+var c = 1 
+while (c <= 20){
+    console.log(`Passo ${c}`)
+    c++
+*/
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula 14/ambiente.js
@@ -0,0 +1,13 @@
+/*var c = 1 
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+// --------------------------
+console.log('Lets starting...')
+for(c = 1; c <=5; c++){
+    console.log(c)
+}
+console.log('END!')
