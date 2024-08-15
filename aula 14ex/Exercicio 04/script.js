cbed971b9cec87c7598c4c69fa0f6b5a7c7c76ae
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-08-14 22:21:18 -0300

    Do the exercise 04

---
+++ aula 14ex/Exercicio 04/script.js
@@ -0,0 +1,21 @@
+function tabuada(){
+    let numero = document.getElementById('txtnumero')
+    let res = document.getElementById('res')
+    
+
+
+    if(numero.value.length == 0){
+        window.alert('Por favor, digite um numero!')
+    } else{
+        let n = Number(numero.value)
+        let c = 1
+        res.innerHTML = ''
+        while(c <= 10 ){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            res.appendChild(item)
+            c++
+        }
+    }
+    
+}
